982e10a4cde4819098d09f018a7e2991a08748ee
harvey_shimizu <harvey_shimizu@moriousa.com>
2021-01-12 10:39:06 -0600

    test

---
+++ mygit.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+read -p "Are you sure to copy your local files to here for comitting? " yn
+
+if [ "$yn" = "y" ] || [ "$yn" = "Y" ]; then
+    $HOME/src/misc/xmyconfig.sh pull $HOME/src/misc/myconfig
+elif [ "n" = "$yn" ] || [ "N" = "$yn" ]; then
+    echo "Skipping to copy your local files to here."
+else
+    echo "Please input yes or not. [y|n] (Case-insensitive): "
+    exit 1
+fi
+
+read -p "Please files to be commted.: " files
+read -p "Please input a message for commiting: " message
+echo $message
+
+git commit $files -m $message
+git push origin
+
