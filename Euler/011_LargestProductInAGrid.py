6f81a9e2619c5559c539e193e63446db800fbc40
Harvey Shimizu <harvey_shimizu@moriousa.com>
2021-02-20 18:42:13 -0600

    updated

--- Euler/011_LargestProductInAGrid.py
+++ Euler/011_LargestProductInAGrid.py
@@ -29,6 +29,7 @@ What is the greatest product of four adjacent numbers in the same direction (up,
 """
 
 import math
+import time
 
 g ='\
 08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08 \
@@ -57,21 +58,63 @@ GN = 20
 gl = g.split()
 h = lambda a,b: math.prod([int(s) for s in a[b:b+OFFS]])
 v = lambda a,b: math.prod([int(s) for s in a[b:b+GN*OFFS:GN]])
-d = lambda a,b: math.prod([int(s) for s in (a[b],a[b+GN+1],a[b+GN*2+2],a[b+GN*3+3])])
-h = lambda a,b: sum([int(s) for s in a[b:b+OFFS]])
-v = lambda a,b: sum([int(s) for s in a[b:b+GN*OFFS:GN]])
-d = lambda a,b: sum([int(s) for s in (a[b],a[b+GN+1],a[b+GN*2+2],a[b+GN*3+3])])
-#print(v(gl, 0))
-#print(h(gl, 0))
-#print(d(gl, 0))
+rd = lambda a,b: math.prod([int(s) for s in (a[b],a[b+GN+1],a[b+GN*2+2],a[b+GN*3+3])])
+od = lambda a,b: math.prod([int(s) for s in (a[b+OFFS],a[b+GN-1+OFFS],a[b+GN*2-2+OFFS],a[b+GN*3-3+OFFS])])
+facotr_return = lambda a,b: [int(s) for s in (a[b+OFFS],a[b+GN-1+OFFS],a[b+GN*2-2+OFFS],a[b+GN*3-3+OFFS])]
+#h = lambda a,b: sum([int(s) for s in a[b:b+OFFS]])
+#v = lambda a,b: sum([int(s) for s in a[b:b+GN*OFFS:GN]])
+#d = lambda a,b: sum([int(s) for s in (a[b],a[b+GN+1],a[b+GN*2+2],a[b+GN*3+3])])
 result = []
-for y in range(0,GN*(GN-OFFS+1),GN):
+factor = ()
+max_value = 0
+t1 = time.time()
+for y in range(0,GN*(GN-OFFS)+1,GN):
     for x in range(0,GN-OFFS+1):
         result.append(h(gl,x+y))
         result.append(v(gl,x+y))
-        result.append(d(gl,x+y))
+        result.append(rd(gl,x+y))
+        result.append(od(gl,x+y))
+#        if max(result) != max_value:
+#            max_value = max(result)
+#            factor = facotr_return(gl, x+y)
+print(f"{max(result)=:5,d}")
+t2 = time.time()
+elapse = (t2 - t1)*10**3
+print (f"{elapse=:.2f}msec")
+#print(result)
+#print(len(result))
+#print(factor)
 
-print(result)
-print(len(result))
-print(max(result))
+# Another solution
+grid = [[0]*23]*3 + [[int(x) for x in line.split()]+[0,0,0] for line in
+'''08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
+49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
+81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
+52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91
+22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80
+24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50
+32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70
+67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21
+24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72
+21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95
+78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92
+16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57
+86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58
+19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40
+04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66
+88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69
+04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36
+20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16
+20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
+01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48'''.split('\n')
+] + [[0]*23]*3
 
+import operator
+from functools import reduce
+t1 = time.time()
+print(max([reduce(operator.mul, [grid[y+n*d[0]][x+n*d[1]] for n in (0,1,2,3)])
+        for x in range(0,20) for y in range(3,23)
+        for d in ((0,1),(1,0),(1,1),(-1,1))]))
+t2 = time.time()
+elapse = (t2 - t1)*10**3
+print (f"{elapse=:.2f}msec")
