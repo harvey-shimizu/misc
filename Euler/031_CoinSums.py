729f2ededae691fb544fef38f284e572e1f1dc40
Harvey Shimizu <harvey_shimizu@moriousa.com>
2021-02-28 00:39:57 -0600

    Euler Program #32

--- Euler/031_CoinSums.py
+++ Euler/031_CoinSums.py
@@ -12,21 +12,91 @@ How many different ways can £2 be made using any number of coins?
 '''
 
 from tqdm import trange, tnrange, tqdm_notebook
+import time
 
 result = list()
 CSUM = 200
-for p100 in trange(0,CSUM//100+1):
-    for p50 in range(0,CSUM//50+1):
-        for p20 in range(0,CSUM//20+1):
-            for p5 in range(0,CSUM//5+1):
-                for p2 in range(0,CSUM//2+1):
-                    for p1 in range(0,CSUM//1+1):
-                        s = 100*p100 + 50*p50 + 20*p20 + 5*p5 + 2*p2 + 1*p1
-                        if s == 200:
-                            result.append({100:p100, 50:p50, 20:p20, 5:p5, 2:p2, 1:p1})
+start = time.time()
+for p100 in range(0,CSUM//100+1):
+    for p50 in range(0,(CSUM-100*p100)//50+1):
+        for p20 in range(0,(CSUM-100*p100-50*p50)//20+1):
+            for p10 in range(0,(CSUM-100*p100-50*p50-20*p20)//10+1):
+                for p5 in range(0,(CSUM-100*p100-50*p50-20*p20-10*p10)//5+1):
+                    for p2 in range(0,(CSUM-100*p100-50*p50-20*p20-10*p10-5*p5)//2+1):
+                        for p1 in range(0,(CSUM-100*p100-50*p50-20*p20-10*p10-5*p5-2*p2)+1):
+                            s = 100*p100 + 50*p50 + 20*p20 + 10*p10 + 5*p5 + 2*p2 + 1*p1
+                            if s == 200:
+                                result.append({100:p100, 50:p50, 20:p20, 10:p10, 5:p5, 2:p2, 1:p1})
+print(len(result)+1)
+end= time.time()
+print(f'{(end-start):,.5f}sec')
 
-for n in result:
-    for i in n.keys():
-        if i == 100:
-            print(n)
-print(len(result))
+start = time.time()
+count = 0
+for p100 in range(CSUM, -1 ,-CSUM):
+    for p50 in range(p100, -1, -100):
+        for p20 in range(p50, -1, -50):
+            for p10 in range(p20, -1, -20):
+                for p5 in range(p10, -1, -10):
+                    for p2 in range(p5, -1, -5):
+                        for p1 in range(p2, -1, -2):
+                            count += 1
+print(count)
+end= time.time()
+print(f'{(end-start)*10**3:,.5f}msec')
+
+# Another solutions
+
+import logging
+
+#logging.basicConfig(level=logging.DEBUG, stream=TqdmStream)
+#log = logging.getLogger(__name__)
+#log.info("loop")
+
+logger = logging.getLogger('LoggingTest')
+logger.setLevel(10)
+sh = logging.StreamHandler()
+logger.addHandler(sh)
+fh = logging.FileHandler('test.log')
+logger.addHandler(fh)
+logger.log(20,'info')
+logger.log(30,'waring')
+logger.log(100,'test')
+
+
+#target = CSUM
+target = 10
+#coinSize = [1,2,5,10,20,50,100,200]
+coinSize = [1,2,5,10]
+ways = [0] * (target+1)
+ways[0] = 1
+
+start = time.time()
+for i in range(len(coinSize)):
+   for j in range(coinSize[i], target+1):
+        ways[j] += ways[j - coinSize[i]]
+#        print(f'ways[{j:3d}]= ways[{j:3d}] + ways[{ways[j-coinSize[i]]:5,d}]')
+        logger.info(f'ways[{j}]= ways[{j}] + ways[{ways[j-coinSize[i]]}]')
+        logger.info(f'coinSize[{i}]={coinSize[i]:3,d} {j=:3,d}, ways[{j}]={ways[j]}')
+#print(ways)
+print(ways[-1])
+end= time.time()
+print(f'{(end-start)*10**3:,.5f}msec')
+
+import logging
+
+import tqdm
+import tqdm_logging_wrapper
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+items = [1, 2, 3]
+items_iter = tqdm.tqdm(items)
+logger.info(f"Items: {items}")
+with tqdm_logging_wrapper.wrap_logging_for_tqdm(items_iter), items_iter:
+    for item in items_iter:
+        logger.info(f"Item: {item}")
+logger.info(f"Items: {items}")
+
+
---
+++ Euler/032_PandigitalProducts.py
@@ -0,0 +1,50 @@
+'''
+Pandigital Products
+Problem #32
+
+We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once; for example, the 5-digit number, 15234, is 1 through 5 pandigital.
+
+The product 7254 is unusual, as the identity, 39 × 186 = 7254, containing multiplicand, multiplier, and product is 1 through 9 pandigital.
+
+Find the sum of all products whose multiplicand/multiplier/product identity can be written as a 1 through 9 pandigital.
+
+HINT: Some products can be obtained in more than one way so be sure to only include it once in your sum.
+'''
+
+from rich.console import Console
+from tqdm import trange
+from rich.progress import track
+
+console = Console()
+
+def checkPandigited(d1, d2):
+    product = d1 * d2
+    check = [s for s in str(product) + str(d1) + str(d2)]
+    if '0' in check:
+        return False
+    i = 0
+    digit = ['1','2','3','4','5','6','7','8','9']
+    isPandigit = True
+    while len(digit) > 0:
+        if len(check) <= i:
+            isPandigit = False
+            break
+        if check[i] in digit:
+            digit.pop(digit.index(check[i]))
+            i += 1
+        else:
+            isPandigit = False
+            break
+    if isPandigit and len(check) < 10:
+        return True
+    return False
+
+total = list()
+for d1 in trange(1,987):
+    for d2 in range(d1,9876):
+        if checkPandigited(d1, d2):
+            if len(str(d1)) <= 2 and not d1*d2 in total:
+                print(f'{d1} x {d2} = {d1*d2}')
+                total.append(d1*d2)
+console.print(sum(total))
+
